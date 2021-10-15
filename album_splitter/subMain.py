14037cc31507c7441ff6d54a4b3d0b4a0f06b0e3
morozov ilya <knpkaa@gmail.com>
2021-10-15 16:35:55 +0300

    ask for URL if not in clipboard

--- album_splitter/subMain.py
+++ album_splitter/subMain.py
@@ -1,9 +1,13 @@
 import os
 import win32clipboard
-#print('Youtube split Downloader'.center(90, '_'))
-#URL = input('Enter youtube url :  ')
+
 win32clipboard.OpenClipboard()
 URL = win32clipboard.GetClipboardData()
 win32clipboard.CloseClipboard()
+
+if not "youtube.com/watch" in URL:
+    print('Youtube split Downloader'.center(90, '_'))
+    URL = input('Enter youtube url :  ')
+    
 command = f'cmd /k python __main__.py -yt {URL}'
 os.system(command)
