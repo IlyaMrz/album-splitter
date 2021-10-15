88eb2a593378710f53996fdb12a3ba40217ed009
morozov ilya <knpkaa@gmail.com>
2021-10-15 16:32:34 +0300

    auto copy url from clipborad

--- album_splitter/subMain.py
+++ album_splitter/subMain.py
@@ -1,5 +1,9 @@
 import os
-print('Youtube split Downloader'.center(90, '_'))
-URL = input('Enter youtube url :  ')
+import win32clipboard
+#print('Youtube split Downloader'.center(90, '_'))
+#URL = input('Enter youtube url :  ')
+win32clipboard.OpenClipboard()
+URL = win32clipboard.GetClipboardData()
+win32clipboard.CloseClipboard()
 command = f'cmd /k python __main__.py -yt {URL}'
 os.system(command)
